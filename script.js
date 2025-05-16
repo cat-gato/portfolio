545971b3a970bad63eb73d89233fae75eb703e2e
cat-gato <contact@eepykat.dev>
2025-05-16 01:31:03 -0700

    added skills section

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-console.log("Cat Script Loaded");
-var navbarAnimation = document.getElementById("navLinks");
-/* function mobileMenu() {
-  
-  console.log(navbarAnimation);
-  if (navbarAnimation.style.display === "block") {
-    console.log("block");
-    navbarAnimation.style.display = "none";
-  } else {
-    console.log("none");
-    navbarAnimation.style.display = "block";
-  }
-}
- */
-
+const navbarAnimation = document.getElementById("navLinks");
 const divElem = document.querySelector("#navLinks");
 const htmlElem = document.querySelector(":root");
 
 
+
+// Function runs when button is pressed
 function mobileMenu() {
+
+  // The menu is open, so close it
   if (divElem.classList[0] === "menu-in") {
     divElem.classList.remove("menu-in");
     divElem.classList.add("menu-out");
+
+
+
+    // or the menu is closed so open it
   } else {
     divElem.classList.remove("menu-out");
     divElem.classList.add("menu-in");
