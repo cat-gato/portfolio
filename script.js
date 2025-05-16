3a7eb4977be66c4ca050074a1efae77731271f04
cat-gato <contact@eepykat.dev>
2025-05-16 00:10:30 -0700

    first responsive update

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 console.log("Cat Script Loaded");
+var navbarAnimation = document.getElementById("navLinks");
+/* function mobileMenu() {
+  
+  console.log(navbarAnimation);
+  if (navbarAnimation.style.display === "block") {
+    console.log("block");
+    navbarAnimation.style.display = "none";
+  } else {
+    console.log("none");
+    navbarAnimation.style.display = "block";
+  }
+}
+ */
 
+const divElem = document.querySelector("#navLinks");
+const htmlElem = document.querySelector(":root");
+
+
+function mobileMenu() {
+  if (divElem.classList[0] === "menu-in") {
+    divElem.classList.remove("menu-in");
+    divElem.classList.add("menu-out");
+  } else {
+    divElem.classList.remove("menu-out");
+    divElem.classList.add("menu-in");
+  }
+}
